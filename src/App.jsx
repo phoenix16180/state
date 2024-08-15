77affec76b82c40deeaa9b17f9c4080e0784f7bd
Speros Misirlakis <6208387+keephopealive@users.noreply.github.com>
2024-08-15 12:42:49 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    // Defining the state variable: count
+    const [count, setCount] = useState(0)
+
+    // Function to update the state variable, we pass the parameter to update the state
+    const updateState = (param) => {
+        setCount((prevState) => {
+            return prevState + param
+        })
+    }
+
+    return (
+        <>
+            <div>
+                {/* Simple state change: */}
+                <h1>Count: {count}</h1>
+                <button onClick={() => setCount(count + 1)}>Increase</button>
+                <button onClick={() => setCount(count - 1)}>Decrease</button>
+
+                {/* Dependable state change via a function call: */}
+                <h1>Count: {count}</h1>
+                <button onClick={() => updateState(+1)}>Increase</button>
+                <button onClick={() => updateState(-1)}>Decrease</button>
+            </div>
+        </>
+    )
+}
+
+export default App
